b09ffe5ea56ae92a1a9432558432ad10ceb9de99
Daniele Napolitano <33985608+danielenapo@users.noreply.github.com>
2019-09-16 22:45:05 +0200

    sas

--- script.js
+++ script.js
@@ -15,31 +15,36 @@ function setup(){
     noiseSeed(seedName);
     background(0);
     //quadrati
-    resolution = 15; //risoluzione dell'mmagine(grandezza dei quadrati)
+    res = 15; //risoluzione dell'mmagine(grandezza dei quadrati)
     quadrati = [];
     //opzon bioma
     waterLevel = 0.35;
-    lGrassLevel = 0.55;
-    randomTreasure = 0.005;
+    lGrassLevel = 0.60;
+    randomTreasure = 0.0005;
     noiseValue = 0;
     //player
     maxHealth=100
-    playerSpeed=3;
-    playerPos=[];   //playerPos is an array of two elements and it determines the pposition of the player in the quadrati matrix
-    playerPos.push(0); //playerPos[0] is x value
-    playerPos.push(0); //playerPos[1] is y value
-    player = new Player(playerPos[0], playerPos[1], resolution, resolution, playerSpeed, maxHealth);
+    playerSpeed=1;
+    player = new Player(0,0, res, res, playerSpeed, maxHealth);
     speedCounter = 6;
-
+    //enemy
+    enemy = new Enemy(10,0, res, res, playerSpeed, maxHealth);
     //canvas
-    width=50*resolution;
-    height=30*resolution;
+    width=30*res;
+    height=15*res;
     createCanvas(width, height);
     noStroke();
 
-    //generate world
-	generateChunk();
-
+    generateChunk(0,0);
+    camera=quadrati;    //quadrati da visualizzare, come una telecamera che vede dall'alto il giocatore
+    generateChunk(0,-height);
+    generateChunk(0,height);
+    generateChunk(width,0);
+    generateChunk(-width,0);
+    generateChunk(width, height);
+    generateChunk(width, -height);
+    generateChunk(-width, height);
+    generateChunk(-width, -height);
 
 }
 
@@ -48,60 +53,72 @@ function setup(){
 
 function draw() {
     controlli();
-
     //stampa quadrati
-    for (var x = 0; x < width * 2; x += resolution) { //per ogni quadrato del canvas
-        for (var y = 0; y < height * 2; y += resolution) {
-            fill(quadrati[x][y].r, quadrati[x][y].g, quadrati[x][y].b);
-            rect(quadrati[x][y].x, quadrati[x][y].y, quadrati[x][y].w, quadrati[x][y].h);
+    for (var x =0; x < width/res; x += 1) { //per ogni quadrato del canvas
+        for (var y = 0; y<height/res; y += 1) {
+            fill(camera[x][y].r, camera[x][y].g, camera[x][y].b);
+            rect(camera[x][y].x*res, camera[x][y].y*res, camera[x][y].w, camera[x][y].h);
         }
     }
 
     //stampa giocatore
     fill(0);
-    rect(player.x, player.y, player.w, player.h);
+    rect((player.x)*res, (player.y)*res, player.w, player.h);
+    //stampa nemico
+    if(enemy.health>0){
+        fill("red");
+        rect((enemy.x)*res, (enemy.y)*res, enemy.w, enemy.h);
+    }
 }
 
 //CONTROLLI CICLICI
 function controlli() {
-    //CONTROLLO TASTI PREMUTI       
-    if (keyIsDown(RIGHT_ARROW) && quadrati[playerPos[0]+resolution][playerPos[1]].r != 24 && speedCounter > player.speed) {
-        playerPos[0]+=resolution;
-        player.x += resolution;
+    //TASTI PREMUTI       
+    if (keyIsDown(RIGHT_ARROW) && quadrati[player.x+1][player.y].r != 24 && speedCounter > player.speed) {
+        player.x += 1;
+        speedCounter = 0;
+       // shiftCamera();
+       // console.log(camera[width/res][player.y])
+    }
+    else if (keyIsDown(LEFT_ARROW) && quadrati[player.x-1][player.y].r != 24 && speedCounter > player.speed){
+        player.x -= 1;
         speedCounter = 0;
     }
-    else if (keyIsDown(LEFT_ARROW) && quadrati[playerPos[0]-resolution][playerPos[1]].r != 24 && speedCounter > player.speed){
-        playerPos[0]-=resolution;
-        player.x -= resolution;
+    else if (keyIsDown(UP_ARROW) && quadrati[player.x][player.y-1].r != 24 &&  speedCounter > player.speed){
+        player.y -= 1;
         speedCounter = 0;
     }
-    else if (keyIsDown(UP_ARROW) && quadrati[playerPos[0]][playerPos[1]-resolution].r != 24 && speedCounter > player.speed){
-        playerPos[1]-=resolution;
-        player.y -= resolution;
-        speedCounter = 0;
-    }
-    else if (keyIsDown(DOWN_ARROW) && quadrati[playerPos[0]][playerPos[1]+resolution].r!= 24 && speedCounter > player.speed){
-        playerPos[1]+=resolution;
-        player.y += resolution;
+    else if (keyIsDown(DOWN_ARROW) && quadrati[player.x][player.y+1].r!= 24 && speedCounter > player.speed){
+        player.y += 1;
         speedCounter = 0;
     }
     speedCounter++;
+
+    //NEMICO
+    if(player.x==enemy.x && player.y==enemy.y){
+        if(keyIsDown(32))
+            enemy.health=0;
+    }
+    if(enemy.health<=0)
+        enemy.x=width+1;
+
 }
 
 
 //GENERA CHUNK
-function generateChunk() {
-    for (var x = 0; x < width * 2; x += resolution) { //per ogni quadrato del canvas
-        quadrati[x]=new Array();
-        for (var y = 0; y < height * 2; y += resolution) {
-            noiseValue = noise(x * 0.005, y * 0.005)
+function generateChunk(cx, cy) {
+    for (var x = (cx/res); x < (width+cx)/res ; x += 1) { //per ogni quadrato del canvas
+        if(!Array.isArray(quadrati[x]))
+            quadrati[x]=new Array();
+        for (var y = (cy/res); y < (height+cy)/res ; y += 1) {
+            noiseValue = noise(x * 0.08, y* 0.08)
             z = 255 * noiseValue; //genera un colore con perlin noise con valori x e y
 
             if (noiseValue > waterLevel && Math.random() <= randomTreasure) {
                 r = 255;
                 g = 255;
                 b = 0;
-                quadrato = new Quadrato(x, y, resolution, resolution, r, g, b, true);
+                quadrato = new Quadrato(x, y, res, res, r, g, b, true);
                 quadrati[x][y]=quadrato;
             }
             else {
@@ -110,7 +127,7 @@ function generateChunk() {
                     g = 64;
                     b = 216;
                 }
-                else if (noiseValue > waterLevel && noise(x * 0.01, y * 0.01) < lGrassLevel) { //altrimenti è terreno
+                else if (noiseValue > waterLevel && noiseValue < lGrassLevel) { //altrimenti è terreno
                     r = 70;
                     g = 127;
                     b = 0;
@@ -120,21 +137,25 @@ function generateChunk() {
                     g = 76;
                     b = 0;
                 }
-                var q = new Quadrato(x, y, resolution, resolution, r, g, b, false);
+                var q = new Quadrato(x, y, res, res, r, g, b, false);
                 quadrati[x][y]=q;
             }
         }
     }
 }
 
-function trovaQuadrato(cercaX, cercaY){
-    var i = 0;
-    for (var y = 0; y < height * 2; y += resolution) { //per ogni quadrato del canvas
-        for (var x = 0; x < width * 2; x += resolution) {         
-            if (y == cercaY && x==cercaX)
-                return quadrati[i].r;
-            i++;
-        }
-    }
 
+function shiftCamera() {
+   for (var x =0; x < width/res; x += 1) { //per ogni quadrato del canvas
+        camera[x].shift();
+        camera[x].push(quadrati[width/res+x][player.y])
+    }
 }
+
+
+
+
+/*LIST OF GOOD SEEDS
+yarumusawa hamiru
+
+*/
